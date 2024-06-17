18fe533574a5bf06bdbbdeb0447c2d526a992126
gabs-stack <gabrielarochadasilva4@gmail.com>
2024-06-17 17:49:58 -0300

    Atualizando

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,27 @@
+public class Operadores {
+    public static void main(String[] args){
+       boolean condicao1=true;
+
+       boolean condicao2=true;
+
+       if(condicao1 && (7 > 4)){
+          System.out.println("As duas condeições são verdadeiras");
+
+       }
+       if(condicao1 || condicao2){
+        System.out.println("Uma das condeições é verdadeira");
+
+     }
+       System.out.println("fim");
+    } 
+}
+
+
+    
+       
+      
+        
+        
+         
+    
+
